f5e58fb69f17ff57e4e90f7a7fca75217ad697fc
mac <mac@macs-MacBook-Pro.local>
2022-02-24 00:58:42 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const oneClick = {
+    createUrl(products, shippingMethods, totalPrice, currency, url, tax) {
+        let oneClickProducts = products.map(product => {
+            let title = product.title.replaceAll('\'', '');
+            let src = product.src.indexOf('&') !== -1? product.src.substring(0, product.src.indexOf('&')): product.src
+            return {
+                id: product.id,
+                title: title.indexOf('&') !== -1? title.substring(0, title.indexOf('&')): title,
+                price: product.amount?product.amount:product.price,
+                quantity: product.quantity,
+                src: src
+            }
+        });
+
+        let oneClickShippingMethods = shippingMethods.map(method => {
+            return {
+                title: method.title,
+                cost: method.cost? method.cost:(method.amount? method.amount:method.price)
+            }
+        });
+        oneClickShippingMethods = JSON.stringify(oneClickShippingMethods);
+        let queryUrl = btoa(encodeURIComponent('products='+JSON.stringify(oneClickProducts)+'&price='+totalPrice+'&currency='+currency+'&url='+url+'/api/one-click/create-order'+'&tax='+tax+'&shipping_methods='+oneClickShippingMethods).replace(/%([0-9A-F]{2})/g, function(match, p1) {
+            return String.fromCharCode('0x' + p1);
+        }));
+        let identityToken = btoa(url);
+
+        return 'https://tezcheckout.qisstpay.com/?identity-token='+identityToken+'&queryUrl='+queryUrl
+    }
+}
+
+module.exports = oneClick;
